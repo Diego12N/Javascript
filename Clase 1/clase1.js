7932070b2491f37bee7a5bfe623f0ac7de8c5299
Diego12N <91293134+Diego12N@users.noreply.github.com>
2021-11-01 23:18:00 -0300

    Add files via upload

---
+++ Clase 1/clase1.js
@@ -0,0 +1,8 @@
+let nombre = prompt("Ingresa tu nombre");
+let apellido = prompt("Ingresa tu apellido");
+let añoActual = 2021;
+alert("Bienvenido" + " " + nombre + " " + apellido + "!");
+console.log("Bienvenido" + " " + nombre + " " + apellido + "!");
+let fechaNas = prompt("Ingresa solo el año de tu nacimiento.");
+alert(nombre + " el año de tu nacimiento es " + fechaNas + ".")
+alert("Según los datos ingresados tu edad aproximada es de " + (añoActual - fechaNas) + " " + "años.")
